990ea09a38195c74d4e1b9176e71a37f86e2279b
James Thomas <codeactor@protonmail.com>
2020-11-02 16:20:41 +0000

    Break down the mtl stack

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,45 @@
 
 module Main where
 
-import Control.Monad.State        (State, evalState)
-import Control.Monad.Trans.Except (runExceptT)
+import Control.Monad.State (State)
+import qualified Control.Monad.State as StateM (evalState)
+import qualified Control.Monad.Trans.Except as Except (runExceptT)
 
 import Err    (CError(..))
 import Mstate (MS(unMS), getState, putState, throwError)
 
 
-dumb :: String -> MS String String
-dumb [] = do
+processData :: String -> MS String String
+processData [] = do
      curr <- getState
      return curr
-dumb (x:xs) = do
+processData (x:xs) = do
      curr <- getState
      case x of
           'x' -> throwError XError
+          'y' -> throwError YError
           _   -> do
                   putState (x:curr)
-                  dumb xs
+                  processData xs
 
 
-dumber :: String -> State String (Either CError String)
-dumber s = runExceptT . unMS $ dumb s
+-- processData :: String -> MS String String
+--
+-- unMS :: MS s a -> ExceptT CError (State s) a
+-- unMS :: MS String String -> ExceptT CError (State s) a
+--
+-- runExceptT :: ExceptT e m a -> m (Either e a)
+-- runExceptT :: ExceptT CError (State s) a -> State String (Either CError String)
+runExceptT :: String -> State String (Either CError String)
+runExceptT s = Except.runExceptT . unMS $ processData s
 
 
-dumbest :: String -> Either CError String
-dumbest s = evalState (runExceptT . unMS $ dumb s) []
+evalState :: String -> Either CError String
+evalState s = StateM.evalState (runExceptT s) []
 
 
 main :: IO ()
 main = do
     word <- getLine
-    let x = dumbest word
+    let x = evalState word
     print x
